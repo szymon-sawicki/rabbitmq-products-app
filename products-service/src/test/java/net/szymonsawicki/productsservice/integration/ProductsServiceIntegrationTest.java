d7fbd2dfee6023bd816133f1760167f717f113e7
szymon-sawicki <szymonsawicki@gmail.com>
2024-09-21 23:04:59 +0200

    improves first test to send messages to rabbitmq direct exchange, introduces second integration test for topic exchange

--- products-service/src/test/java/net/szymonsawicki/productsservice/integration/ProductsServiceIntegrationTest.java
+++ products-service/src/test/java/net/szymonsawicki/productsservice/integration/ProductsServiceIntegrationTest.java
@@ -1,12 +1,16 @@
 package net.szymonsawicki.productsservice.integration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.math.BigDecimal;
+import net.szymonsawicki.productsservice.model.Product;
 import net.szymonsawicki.productsservice.model.type.Category;
 import net.szymonsawicki.productsservice.utils.TestUtils;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
@@ -21,6 +25,8 @@ public class ProductsServiceIntegrationTest {
   @Container @ServiceConnection
   static RabbitMQContainer rabbitMQContainer = new RabbitMQContainer("rabbitmq:3-management");
 
+  @Autowired private RabbitTemplate rabbitTemplate;
+
   @LocalServerPort private int port;
 
   @BeforeAll
@@ -34,16 +40,30 @@ public class ProductsServiceIntegrationTest {
   }
 
   @Test
-  protected void shouldRecieveOneProductInEachCategory() {
+  protected void shouldRecievedProductInEachCategoryDirectExchange() throws InterruptedException {
 
     // arrange
 
-    var bookProduct =
+    var bookProduct1 =
         TestUtils.createProduct(1L, "Harry Poter", Category.BOOKS, new BigDecimal("7"));
+    var bookProduct2 =
+        TestUtils.createProduct(1L, "Harry Poter", Category.BOOKS, new BigDecimal("7"));
+
     var clothingProduct =
         TestUtils.createProduct(2L, "Shirt", Category.CLOTHINGS, new BigDecimal("12"));
     var electronicsProduct =
         TestUtils.createProduct(3L, "Laptop", Category.ELECTRONICS, new BigDecimal("200"));
+
+    var exchange = "x.products-direct";
+
+    // act
+
+    sendProductToRabbitMQ(exchange, bookProduct1);
+    sendProductToRabbitMQ(exchange, bookProduct2);
+    sendProductToRabbitMQ(exchange, clothingProduct);
+    sendProductToRabbitMQ(exchange, electronicsProduct);
+
+    Thread.sleep(1000);
 
     // act
 
@@ -52,5 +72,48 @@ public class ProductsServiceIntegrationTest {
     // assert
 
     Assertions.assertThat(response.getBody()).isNotNull();
+    Assertions.assertThat(response.getBody().getBookProducts()).isNotNull().hasSize(2);
+    Assertions.assertThat(response.getBody().getClothingProducts()).isNotNull().hasSize(1);
+    Assertions.assertThat(response.getBody().getElectronicsProducts()).isNotNull().hasSize(1);
+  }
+
+  @Test
+  protected void shouldRecievedProductInEachCategory() throws InterruptedException {
+
+    // arrange
+
+    var bookProduct1 =
+        TestUtils.createProduct(1L, "Harry Poter", Category.BOOKS, new BigDecimal("7"));
+    var bookProduct2 =
+        TestUtils.createProduct(1L, "Harry Poter", Category.BOOKS, new BigDecimal("7"));
+
+    var clothingProduct =
+        TestUtils.createProduct(2L, "Shirt", Category.CLOTHINGS, new BigDecimal("30"));
+
+    var exchange = "x.products-topic";
+
+    // act
+
+    sendProductToRabbitMQ(exchange, bookProduct1);
+    sendProductToRabbitMQ(exchange, bookProduct2);
+    sendProductToRabbitMQ(exchange, clothingProduct);
+
+    Thread.sleep(1000);
+
+    var response = TestUtils.makeSummaryRequest(port);
+
+    // assert
+
+    Assertions.assertThat(response.getBody().getExpensiveClothingProducts()).isNotNull().hasSize(1);
+    Assertions.assertThat(response.getBody().getCheapProducts()).isNotNull().hasSize(2);
+  }
+
+  private void sendProductToRabbitMQ(String exchange, Product product) {
+    try {
+      var jsonProduct = new ObjectMapper().writeValueAsString(product);
+      rabbitTemplate.convertAndSend(exchange, null, jsonProduct);
+    } catch (Exception e) {
+      throw new IllegalStateException(e);
+    }
   }
 }
