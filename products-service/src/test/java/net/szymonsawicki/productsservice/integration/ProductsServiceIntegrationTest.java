33e703d08d86d511caa42238d14c960a1c17b1d2
szymon-sawicki <szymonsawick@gmail.com>
2024-10-04 07:55:54 +0200

    tries to fix integration test (still without success)

--- products-service/src/test/java/net/szymonsawicki/productsservice/integration/ProductsServiceIntegrationTest.java
+++ products-service/src/test/java/net/szymonsawicki/productsservice/integration/ProductsServiceIntegrationTest.java
@@ -2,36 +2,63 @@ package net.szymonsawicki.productsservice.integration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.math.BigDecimal;
+import net.szymonsawicki.productsservice.ProductsService;
 import net.szymonsawicki.productsservice.model.Product;
 import net.szymonsawicki.productsservice.model.type.Category;
 import net.szymonsawicki.productsservice.utils.TestUtils;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
+@Profile("test")
 public class ProductsServiceIntegrationTest {
 
   @Container @ServiceConnection
   static RabbitMQContainer rabbitMQContainer = new RabbitMQContainer("rabbitmq:3-management");
 
   @Autowired private RabbitTemplate rabbitTemplate;
+  @Autowired private ProductsService productsService;
 
   @LocalServerPort private int port;
 
+  @DynamicPropertySource
+  static void registerProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.rabbitmq.host", rabbitMQContainer::getHost);
+    registry.add("spring.rabbitmq.port", rabbitMQContainer::getAmqpPort);
+    registry.add("spring.rabbitmq.username", rabbitMQContainer::getAdminUsername);
+    registry.add("spring.rabbitmq.password", rabbitMQContainer::getAdminPassword);
+    // Add these lines to ensure proper connection
+    registry.add("spring.rabbitmq.connection-timeout", () -> "5000");
+    registry.add("spring.rabbitmq.requested-heartbeat", () -> "30");
+  }
+
   @BeforeAll
-  static void beforeAll() {
+  static void beforeAll() throws InterruptedException {
     rabbitMQContainer.start();
+    System.out.println("RabbitMQ is running on port: " + rabbitMQContainer.getMappedPort(5672));
+
+    Thread.sleep(50000);
+  }
+
+  @BeforeEach
+  void beforeEach() {
+    prepareTestData();
   }
 
   @AfterAll
@@ -42,28 +69,16 @@ public class ProductsServiceIntegrationTest {
   @Test
   protected void shouldRecievedProductInEachCategoryDirectExchange() throws InterruptedException {
 
+    // TODO WIP - this test is not working as expected, connection to RabbitMQ from service should
+    // be fixed
+
     // arrange
 
-    var bookProduct1 =
-        TestUtils.createProduct(1L, "Harry Poter", Category.BOOKS, new BigDecimal("7"));
-    var bookProduct2 =
-        TestUtils.createProduct(1L, "Harry Poter", Category.BOOKS, new BigDecimal("7"));
-
-    var clothingProduct =
-        TestUtils.createProduct(2L, "Shirt", Category.CLOTHINGS, new BigDecimal("12"));
-    var electronicsProduct =
-        TestUtils.createProduct(3L, "Laptop", Category.ELECTRONICS, new BigDecimal("200"));
-
-    var exchange = "x.products-direct";
+    RabbitAdmin rabbitAdmin = new RabbitAdmin(rabbitTemplate);
 
     // act
 
-    sendProductToRabbitMQ(exchange, bookProduct1);
-    sendProductToRabbitMQ(exchange, bookProduct2);
-    sendProductToRabbitMQ(exchange, clothingProduct);
-    sendProductToRabbitMQ(exchange, electronicsProduct);
-
-    Thread.sleep(1000);
+    Thread.sleep(10000);
 
     // act
 
@@ -72,8 +87,8 @@ public class ProductsServiceIntegrationTest {
     // assert
 
     Assertions.assertThat(response.getBody()).isNotNull();
-    Assertions.assertThat(response.getBody().getBookProducts()).isNotNull().hasSize(2);
     Assertions.assertThat(response.getBody().getClothingProducts()).isNotNull().hasSize(1);
+    Assertions.assertThat(response.getBody().getBookProducts()).isNotNull().hasSize(2);
     Assertions.assertThat(response.getBody().getElectronicsProducts()).isNotNull().hasSize(1);
   }
 
@@ -82,21 +97,7 @@ public class ProductsServiceIntegrationTest {
 
     // arrange
 
-    var bookProduct1 =
-        TestUtils.createProduct(1L, "Harry Poter", Category.BOOKS, new BigDecimal("7"));
-    var bookProduct2 =
-        TestUtils.createProduct(1L, "Harry Poter", Category.BOOKS, new BigDecimal("7"));
-
-    var clothingProduct =
-        TestUtils.createProduct(2L, "Shirt", Category.CLOTHINGS, new BigDecimal("30"));
-
     var exchange = "x.products-topic";
-
-    // act
-
-    sendProductToRabbitMQ(exchange, bookProduct1);
-    sendProductToRabbitMQ(exchange, bookProduct2);
-    sendProductToRabbitMQ(exchange, clothingProduct);
 
     Thread.sleep(1000);
 
@@ -108,10 +109,30 @@ public class ProductsServiceIntegrationTest {
     Assertions.assertThat(response.getBody().getCheapProducts()).isNotNull().hasSize(2);
   }
 
+  private void prepareTestData() {
+
+    var exchange = "x.products-direct";
+
+    var bookProduct1 =
+        TestUtils.createProduct(1L, "Harry Poter", Category.BOOKS, new BigDecimal("7"));
+    var bookProduct2 =
+        TestUtils.createProduct(1L, "Harry Poter", Category.BOOKS, new BigDecimal("7"));
+
+    var clothingProduct =
+        TestUtils.createProduct(2L, "Shirt", Category.CLOTHINGS, new BigDecimal("12"));
+    var electronicsProduct =
+        TestUtils.createProduct(3L, "Laptop", Category.ELECTRONICS, new BigDecimal("200"));
+
+    sendProductToRabbitMQ(exchange, bookProduct1);
+    sendProductToRabbitMQ(exchange, bookProduct2);
+    sendProductToRabbitMQ(exchange, clothingProduct);
+    sendProductToRabbitMQ(exchange, electronicsProduct);
+  }
+
   private void sendProductToRabbitMQ(String exchange, Product product) {
     try {
       var jsonProduct = new ObjectMapper().writeValueAsString(product);
-      rabbitTemplate.convertAndSend(exchange, null, jsonProduct);
+      rabbitTemplate.convertAndSend(exchange, "", jsonProduct);
     } catch (Exception e) {
       throw new IllegalStateException(e);
     }
